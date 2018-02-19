314150016a1a62371df8a82029dac682a0dc3e65
Jonny Westfall <jwestfa0@email.cpcc.edu>
2018-02-19 10:35:10 -0500

    Proto
    Just a few simple PC elements. Opening window, start menu, taskbar.

---
+++ 90sHackerMovie/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager gM;
+
+	[Header("Start Menu")]
+	public GameObject startPanel;
+
+	[Header("Program")]
+	public GameObject program;
+
+	void Awake()
+	{
+		if(!gM)
+			gM = this;
+		else
+			Destroy(this.gameObject);
+	}
+}
---
+++ 90sHackerMovie/Assets/Scripts/Programs.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Programs : MonoBehaviour
+{
+    bool active;
+
+    public GameObject program;
+	public GameObject[] programWindows;
+
+    void Start()
+    {
+		program = GameManager.gM.program;
+    }
+
+    void Update()
+    {
+    }
+
+    public void ProgramButtonOn()
+    {
+		program.SetActive(true);
+    }
+
+	public void ProgramWindowOpen()
+	{
+		programWindows[0].SetActive(true);
+	}
+   
+}
