96a89f14ee3190836948bc0a9e04e6585074b9f1
Preston Rockholt <prockho0@email.cpcc.edu>
2018-03-07 21:49:14 -0500

    We need files to have a program

---
+++ Assets/Scripts/Programs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Programs : MonoBehaviour
+{
+
+    public GameObject[] program;
+    public GameObject[] programWindows;
+
+
+    public void OpenProgram(int index)
+    {
+        programWindows[index].SetActive(true);
+        program[index].SetActive(true);
+    }
+
+    public void CloseProgram(int index)
+    {
+      program[index].SetActive(false);
+      programWindows[index].SetActive(false);
+
+    }
+
+}
